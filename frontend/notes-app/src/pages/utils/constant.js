1efea9774e20192304b386976e43182f8de227b1
Sohaib0786 <shoaibqu7714@gmail.com>
2025-06-06 15:44:29 +0530

    Deployment frontend

--- frontend/notes-app/src/pages/utils/constant.js
+++ frontend/notes-app/src/pages/utils/constant.js
@@ -1,1 +1,9 @@
-export const BASE_URL = 'http://localhost:8000'
+ let IS_PROD = true;
+ 
+ const BASE_URL = IS_PROD ?
+        "https://stacknotes-appbackend.onrender.com" :
+       "http://localhost:8000" ;
+      
+ 
+
+ export default BASE_URL;
