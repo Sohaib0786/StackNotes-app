54217c4ff42810cf99d6ddd5b8f2b174ffe97cce
Sohaib0786 <shoaibqu7714@gmail.com>
2025-06-06 23:10:17 +0530

    deploy changes

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,26 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home/Home';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
 
-
-const routes = (
-    <Router>
-      <Routes>
-        <Route path='/' exact element={<SignUp/>}/>
-        <Route path="/dashboard" exact element= {<Home />} />
-        <Route path="/login" exact element= {<Login/>}/>
-        <Route path="/signup" exact element ={<SignUp/>}/>
-
-        
-      </Routes>
-    </Router>
-
-);
-
 const App = () => {
-  return <div>{routes}</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
